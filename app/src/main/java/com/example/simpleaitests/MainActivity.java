b98822f10e70e6c88c74169ddc24240280d2c73f
Jeramy Dichiera <jeramy.dichiera@ncino.com>
2019-12-28 10:45:55 -0500

    Launch second activity on button click

--- app/src/main/java/com/example/simpleaitests/MainActivity.java
+++ app/src/main/java/com/example/simpleaitests/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.simpleaitests;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+
+    public void openNewActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
